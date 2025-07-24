29f110879fea5fb62dbc1ea459d0a5bbc5cf975e
pujaPatil169 <177208178+pujaPatil169@users.noreply.github.com>
2025-07-24 10:56:40 +0530

    bug fixes and added paytm payment gateway

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -275,7 +275,7 @@ const Add = ({ token }) => {
         }
       });
 
-      const response = await axios.post(backendUrl + "/product/add", formDataToSend, {
+      const response = await axios.post(backendUrl.base + "/product/add", formDataToSend, {
         headers: { token }
       });
 
