8ba3c83c610e291bc0ba97cbdeb8a62e29e8805f
Ashish Raj <raj.ashish2808@gmail.com>
2025-08-01 15:26:49 +0530

    admin path change

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin/',          // 👈 absolutely essential for production
+  base: '/admin',          // 👈 absolutely essential for production
   plugins: [react()],
   server: {
     port: 5173              // for local dev only, no effect in prod
